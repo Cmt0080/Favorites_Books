53b8af28cf787f8219ce28d7b5f862588d655870
Connor Townsend <cmt0080@auburn.edu>
2026-03-29 09:14:08 -0500

    Code Written for BookRowView.swift

--- Favorites_Completed/Views/BookRowView.swift
+++ Favorites_Completed/Views/BookRowView.swift
@@ -4,4 +4,41 @@
 //
 //  Created by Connor Townsend on 3/29/26.
 //
+import SwiftUI
 
+struct BookRowView: View {
+    
+    let book: BookModel
+    @EnvironmentObject private var favorites: FavoritesViewModel
+    
+    var body: some View {
+        HStack {
+            Text("📖")
+                .font(.title2)
+            VStack(alignment: .leading) {
+                Text(book.bookTitle)
+                    .font(.body)
+                Text(book.bookAuthor)
+                    .font(.caption)
+                    .foregroundStyle(.secondary)
+            }
+            
+            Spacer()
+            
+            Button(action:{
+                favorites.toggleFavoriteBook(book: book)
+                
+            }) {
+                Image(systemName: book.isFavorite ? "heart.fill" : "heart")
+                    .foregroundStyle(book.isFavorite ? .red : .gray)
+                
+            }
+        }
+        .padding(.vertical,4)
+    }
+}
+
+#Preview {
+    BookRowView(book: BookModel(id: 1, bookTitle: "The Lord of the Rings", bookAuthor: "J.R.R. Tolkien"))
+        .environmentObject(FavoritesViewModel())
+}
